46b01d784db323803563078cb4aef1b1e50cd5e2
Paulo Monezi <paulo.monezi@icloud.com>
2022-10-30 20:54:06 -0400

    sprites added

--- index.js
+++ index.js
@@ -3,8 +3,31 @@ const c = canvas.getContext('2d')
 
 canvas.width = 1024
 canvas.height = 576
-console.log(c)
 
 c.fillStyle = 'white'
 c.fillRect(0, 0, canvas.width, canvas.height)
+
+const playerImage = new Image()
+playerImage.src = './src/images/sprites/player/playerDown.png'
+
+const image = new Image()
+image.src = './src/images/PelletTown.png'
+
+
+image.onload = () => {
+    c.drawImage(image, -735, -600)
+    c.drawImage( //Those 4 first args determine the x, y axis from where to start and where to finish cropping the image (its a spritesheet, with four images)
+        playerImage,
+        0,
+        0,
+        playerImage.width / 4,
+        playerImage.height,
+        // Those 4 last args, determine the actual position of the player
+        canvas.width / 2 - playerImage.width / 4 / 2,
+        canvas.height / 2 - playerImage.height / 2,
+        playerImage.width / 4,
+        playerImage.height
+    )
+}
+
 
