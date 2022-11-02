380a2a9d314fa44f3147f8acdeb4f315792bea08
Paulo Monezi <paulo.monezi@icloud.com>
2022-11-01 21:41:59 -0400

    working on boundaries/collisions

--- index.js
+++ index.js
@@ -4,8 +4,43 @@ const c = canvas.getContext('2d')
 canvas.width = 1024
 canvas.height = 576
 
-c.fillStyle = 'white'
-c.fillRect(0, 0, canvas.width, canvas.height)
+const collisionsMap = []
+for (let i = 0; i < collisions.length; i += 70) {
+    collisionsMap.push(collisions.slice(i, 70 + i))
+}
+
+class Boundary {
+    static width = 48
+    static height = 48
+    constructor({ position }) {
+        this.position = position
+        this.width = 48
+        this.height = 48
+    }
+
+    draw() {
+        c.fillStyle = 'red'
+        c.fillRect(this.position.x, this.position.y, this.width, this.height)
+    }
+}
+
+const boundaries = []
+const offset = {
+    x: -735,
+    y: -600
+}
+
+collisionsMap.forEach((row, i) => {
+    row.forEach((symbol, j) => {
+        if (symbol == 1025)
+            boundaries.push(new Boundary({
+                position: {
+                    x: j * Boundary.width + offset.x,
+                    y: i * Boundary.height + offset.y
+                }
+            }))
+    })
+})
 
 const playerImage = new Image()
 playerImage.src = './src/images/sprites/player/playerDown.png'
@@ -26,8 +61,8 @@ class Sprite {
 
 const background = new Sprite({
     position: {
-        x: -735,
-        y: -600
+        x: offset.x,
+        y: offset.y
     },
     image: image
 })
@@ -47,9 +82,21 @@ const keys = {
     }
 }
 
+const testBoundary = new Boundary({
+    position: {
+        x: 400,
+        y: 400
+    }
+})
+
+const movables = [background, testBoundary]
 function animate() {
     window.requestAnimationFrame(animate)
     background.draw()
+    // boundaries.forEach(boundary => {
+    //     boundary.draw()
+    // })
+    testBoundary.draw()
     c.drawImage(
         playerImage,
         0,
@@ -62,10 +109,18 @@ function animate() {
         playerImage.height
     )
 
-    if (keys.w.pressed && lastKey === 'w') background.position.y += 3
-    else if (keys.a.pressed && lastKey === 'a') background.position.x += 3
-    else if (keys.s.pressed && lastKey === 's') background.position.y += -3
-    else if (keys.d.pressed && lastKey === 'd') background.position.x += -3
+    if (keys.w.pressed && lastKey === 'w') {
+        movables.forEach(movable => { movable.position.y += 3 })
+    }
+    else if (keys.a.pressed && lastKey === 'a')  {
+        movables.forEach(movable => { movable.position.x += 3 })
+    }
+    else if (keys.s.pressed && lastKey === 's')  {
+        movables.forEach(movable => { movable.position.y -= 3 })
+    }
+    else if (keys.d.pressed && lastKey === 'd')  {
+        movables.forEach(movable => { movable.position.x -= 3 })
+    }
 }
 animate()
 
@@ -107,4 +162,3 @@ window.addEventListener('keyup', (e) => {
             break
     }
 })
-
