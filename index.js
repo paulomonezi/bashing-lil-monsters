f8399afcdd61dacd46729b3b48e7e662d79f5e0e
Paulo Monezi <paulo.monezi@icloud.com>
2022-11-01 20:48:02 -0400

    collisions json imported

--- index.js
+++ index.js
@@ -62,26 +62,31 @@ function animate() {
         playerImage.height
     )
 
-    if (keys.w.pressed) {
-        background.position.y = background.position.y + 3
-        console.log(background.position.y)
-    }
+    if (keys.w.pressed && lastKey === 'w') background.position.y += 3
+    else if (keys.a.pressed && lastKey === 'a') background.position.x += 3
+    else if (keys.s.pressed && lastKey === 's') background.position.y += -3
+    else if (keys.d.pressed && lastKey === 'd') background.position.x += -3
 }
 animate()
 
+let lastKey = ''
 window.addEventListener('keydown', (e) => {
     switch (e.key) {
         case 'w':
             keys.w.pressed = true
+            lastKey = 'w'
             break
         case 'a':
             keys.a.pressed = true
+            lastKey = 'a'
             break
         case 's':
             keys.s.pressed = true
+            lastKey = 's'
             break
         case 'd':
             keys.d.pressed = true
+            lastKey = 'd'
             break
     }
 })
